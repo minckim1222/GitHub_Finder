9907048ea12b21cfb3a37ea8ff33067945e5c7d2
Min Kim <min.c.kim@gmail.com>
2020-03-07 18:54:53 -0600

    Refactored Searsch to use Hooks

--- src/users/Search.js
+++ src/users/Search.js
@@ -1,50 +1,48 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export class Search extends Component {
-  state = {
-    text: ""
-  };
-  onChange = e =>
-    this.setState({
-      [e.target.name]: e.target.value
-    });
+const Search = ({
+  showAlert,
+  searchUsers,
+  clearUsers,
+  usersLoaded,
+  onChange,
+  onSubmit
+}) => {
+  const [text, setText] = useState("");
+  onChange = e => setText(e.target.value);
 
   onSubmit = e => {
     e.preventDefault();
-    if (this.state.text === "") {
-      this.props.showAlert("Please enter a username", "alert-light");
+    if (text === "") {
+      showAlert("Please enter a username", "alert-light");
     } else {
-      this.props.searchUsers(this.state.text);
-      this.setState({ text: "" });
+      searchUsers(text);
+      setText("");
     }
   };
-  render() {
-    const { usersLoaded, clearUsers } = this.props;
-    const { text } = this.state;
-    return (
-      <div>
-        <form onSubmit={this.onSubmit} className='form'>
-          <input
-            type='text'
-            name='text'
-            placeholder='Search users.'
-            value={text}
-            onChange={this.onChange}
-          />
-          <input
-            type='submit'
-            value='Search'
-            className='btn btn-dark btn-block'
-          />
-        </form>
-        {usersLoaded && (
-          <button className='btn btn-light btn-block' onClick={clearUsers}>
-            Clear
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className='form'>
+        <input
+          type='text'
+          placeholder='Search users.'
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type='submit'
+          value='Search'
+          className='btn btn-dark btn-block'
+        />
+      </form>
+      {usersLoaded && (
+        <button className='btn btn-light btn-block' onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
 
 export default Search;
